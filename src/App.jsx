052b757c32a69e619d460934f28164be579a9b2f
arjunjoshua <arjunjoshua@gmail.com>
2024-12-09 16:16:41 +0000

    Updated the package and app for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {useState} from 'react'
 import './App.css'
+import secret_santa_list from '/scripts/secret_santa_list.json'
 
 function App() {
     const [name, setName] = useState('')
     const [isNameVisible, setIsNameVisible] = useState(false)
+    const [displayText, setDisplayText] = useState('')
 
-        const handleNameSubmit = () => {
+    const handleNameSubmit = () => {
         // Check if the input is empty
         if (name.trim() === '') {
             alert("Don't QA test me. Enter your name.");
@@ -14,30 +16,37 @@ function App() {
 
         // Show the name display
         setIsNameVisible(true);
+
+        // Check if the name is in the secret santa list
+        if (secret_santa_list[name.toLowerCase()]) {
+            let ss_name = secret_santa_list[name.toLowerCase()];
+            setDisplayText(`You are the Secret Santa for ${ss_name.charAt(0).toUpperCase() + ss_name.slice(1)}.`);
+        } else {
+            setDisplayText(`${name} is not in the Secret Santa list. Please contact Arjun.`);
+        }
     };
 
     const handleInputChange = (event) => {
         let name_temp = event.target.value;
-        setName(name_temp.charAt(0).toUpperCase() + name_temp.slice(1));
+        setName((name_temp.charAt(0).toUpperCase() + name_temp.slice(1).trim()));
     };
     return (
         <>
             <span className="star">&#9733;</span>
 
             <h1 className="star_header">
-                Fun
+                F<span className="star_header_middle">u</span>n
             </h1>
             <h1 className="tree_top">
-                Lovin
+                Lo<span className="tree_top_middle">v</span>in
             </h1>
             <h1 className="tree_middle">
-                Family
+                <span className="tree_middle_left">F</span>am<span className="tree_middle_right">i</span>ly
             </h1>
             <h1 className="tree_bottom">
-                Secret Santa
+                S<span className="tree_bottom_left">e</span>cret Sant<span className="tree_bottom_right">a</span>
             </h1>
             <div>
-                {/* create an input field and submit button */}
                 <input id="ss_name"
                        className="name_input"
                        type="text"
@@ -49,7 +58,9 @@ function App() {
             <button className="name_submit" onClick={handleNameSubmit}>Submit</button>
 
             {/* display the name conditionally */}
-            {isNameVisible && <h2 id="name_display">Hello {name}</h2>}
+            {isNameVisible && <div>
+                <h2 id="name_display"><span>🎅</span>   {displayText}    <span>🎅</span></h2>
+            </div>}
         </>
     )
 }
